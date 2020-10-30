70c7a5dafa0768cf630f7072857a9592d82e0b80
Sabalvaro, Enrik P <enrik.p.sabalvaro@accenture.com>
2020-10-30 21:48:13 +0800

    :rocket: feat: added email

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,6 +1,49 @@
 import React from "react";
+import autoBind from "react-autobind";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+
+    this.state = {
+      email: "",
+      name: "",
+      message: "",
+    };
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const requestBody = {
+      name: this.state.name,
+      email: this.state.email,
+      message: this.state.message,
+    };
+
+    axios
+      .post("https://g-eggs-server.tech/email", requestBody)
+      .then((response) => {
+        if (response) {
+          this.setState({
+            email: "",
+            name: "",
+            message: "",
+          });
+          toast.success("🚀 Email Sent!");
+        } else {
+          toast.error("Something went wrong!");
+        }
+      });
+  }
+
   render() {
     return (
       <section className="wedo py-5" id="contact">
@@ -16,30 +59,36 @@ class Contact extends React.Component {
             </p>
           </div>
           <div className="contact_grid_right mt-5 mx-auto text-center">
-            <form action="#" method="post">
+            <form action="#" method="post" onSubmit={this.handleSubmit}>
               <div className="row contact_top">
                 <div className="col-sm-6">
                   <input
                     type="text"
-                    name="Name"
+                    name="name"
                     placeholder="Name"
+                    value={this.state.name}
+                    onChange={this.handleChange}
                     required=""
                   />
                 </div>
                 <div className="col-sm-6">
                   <input
                     type="email"
-                    name="Email"
-                    placeholder="Email"
+                    name="email"
+                    value={this.state.email}
+                    placeholder="Your Email"
+                    onChange={this.handleChange}
                     required=""
                   />
                 </div>
               </div>
               <textarea
-                name="Message"
+                name="message"
                 onfocus="this.value = '';"
                 onblur="if (this.value == '') {this.value = 'Message...';}"
                 required=""
+                value={this.state.message}
+                onChange={this.handleChange}
               >
                 Message...
               </textarea>
@@ -48,6 +97,7 @@ class Contact extends React.Component {
               <div className="clearfix"> </div>
             </form>
           </div>
+          <ToastContainer />
         </div>
       </section>
     );
